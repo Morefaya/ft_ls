55b85df0e3b04343385f90de3c25c33060b0ca2b
Jean-charles CAZAKO <jcazako@e1r8p14.42.fr>
2016-02-06 23:03:14 +0100

    data work

--- include/ft_ls.h
+++ include/ft_ls.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcazako <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/06 21:28:40 by jcazako           #+#    #+#             */
+/*   Updated: 2016/02/06 21:29:01 by jcazako          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_LS_H
 # define FT_LS_H
 
@@ -7,13 +19,14 @@
 
 typedef struct	s_ls
 {
-	char	*name;
-	int	nb_link;
-	char	*rights;
-	char	*u_name;
-	char	*g_name;
-	size_t	size;
-	char	*time;
-}		t_ls;
+	char		*name;
+	int			nb_slink;
+	char		type;
+	char		*rights;
+	char		*u_name;
+	char		*g_name;
+	size_t		size;
+	char		*time;
+}				t_ls;
 
 #endif
--- src/get_data.c
+++ src/get_data.c
@@ -1,13 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_data.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcazako <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/06 20:19:34 by jcazako           #+#    #+#             */
+/*   Updated: 2016/02/06 22:09:28 by jcazako          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_ls.h"
 
-t_ls	*get_name(DIR *f_dir)
+t_list	*get_data(t_list **list, DIR *f_dir)
 {
-	static t_list	*list;
+	t_list			*lst_tmp;
+	t_ls			content;
 	struct dirent	*f_dirent;
+	struct stat		*f_stat;
 
-	
-	if ((f_dirent = readdir(f_dir)))
+	while ((f_dirent = readdir(f_dir)))
 	{
-		f_dirent->name;
+		if (!(content.name = ft_strdup(f_dirent->d_name)));
+			return (NULL);
+		if (!(lst_tmp = lst_new(&content, sizeof(content))))
+		{
+			free(content.name);
+			return (NULL);
+		}
+		ft_lstadd(list, lst_tmp);
 	}
 }
