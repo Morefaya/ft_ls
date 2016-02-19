fc0ad7d769e9f427b04369108a208cd0143588fb
Jean-charles CAZAKO <jcazako@e1r3p18.42.fr>
2016-02-19 18:45:55 +0100

    get putlf

--- src/print_lf.c
+++ src/print_lf.c
@@ -6,7 +6,7 @@
 /*   By: jcazako <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/18 16:49:47 by jcazako           #+#    #+#             */
-/*   Updated: 2016/02/19 18:25:35 by jcazako          ###   ########.fr       */
+/*   Updated: 2016/02/19 18:43:45 by jcazako          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,23 @@ void	print_link(char *link)
 	ft_putchar('\n');
 }
 
+void	putlf(t_list *lst, t_max buff)
+{
+	ft_putchar(((t_ls*)(lst->content))->type);
+	ft_putendchar(((t_ls*)(lst->content))->rights, ' ');
+	ft_putstr_right(ft_itoa(((t_ls*)(lst->content))->nb_hlink), buff.mx_hl);
+	ft_putchar(' ');
+	ft_putstr_left(((t_ls*)(lst->content))->u_name, buff.mx_un);
+	ft_putstr("  ");
+	ft_putstr_left(((t_ls*)(lst->content))->g_name, buff.mx_gn);
+	ft_putstr("  ");
+	ft_putstr_right(ft_itoa(((t_ls*)(lst->content))->size), buff.mx_sz);
+	ft_putchar(' ');
+	ft_putendchar(((t_ls*)(lst->content))->time, ' ');
+	ft_putstr(((t_ls*)(lst->content))->name);
+	print_link(((t_ls*)(lst->content))->link);
+}
+
 void	print_lf(t_list *lst)
 {
 	t_max	buff;
@@ -32,20 +49,7 @@ void	print_lf(t_list *lst)
 	ft_putchar('\n');
 	while (lst)
 	{
-		ft_putchar(((t_ls*)(lst->content))->type);
-		ft_putendchar(((t_ls*)(lst->content))->rights, ' ');
-		ft_putstr_right(ft_itoa(((t_ls*)(lst->content))->nb_hlink), buff.mx_hl);
-		ft_putchar(' ');
-		ft_putstr_left(((t_ls*)(lst->content))->u_name, buff.mx_un);
-		ft_putchar(' ');
-		ft_putstr_left(((t_ls*)(lst->content))->g_name, buff.mx_gn);
-		ft_putchar(' ');
-		ft_putstr_right(ft_itoa(((t_ls*)(lst->content))->size), buff.mx_sz);
-		ft_putchar(' ');
-		ft_putendchar(((t_ls*)(lst->content))->time, ' ');
-		ft_putchar(' ');
-		ft_putstr(((t_ls*)(lst->content))->name);
-		print_link(((t_ls*)(lst->content))->link);
+		putlf(lst, buff);
 		lst = lst->next;
 	}
 }
